238937c8cbff24a56ca9cea378c4bd8ccee0f0b0
Abhik Ray <A30000943@agl.com.au>
2019-04-23 23:02:29 +1000

    Test case refactoring.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to agl-code-test!');
+    expect(compiled.querySelector('h1').textContent).toContain('AGL Code Test');
   });
 });
